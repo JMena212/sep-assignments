be953355d842851f90985e654dc604cb44ef5aa4
Jose Mena <josemena0114@gmail.com>
2018-12-12 16:14:05 -0500

    stack works

--- 01-data-structures/02-stacks-and-queues/mystack/mystack.rb
+++ 01-data-structures/02-stacks-and-queues/mystack/mystack.rb
@@ -7,11 +7,17 @@ class MyStack
   end
 
   def push(item)
+    self.top = item
+    @stack << item
   end
 
   def pop
+    last_in = @stack.delete_at(-1)
+    self.top = @stack[-1]
+    return last_in
   end
 
   def empty?
+    @stack.length == 0
   end
 end
