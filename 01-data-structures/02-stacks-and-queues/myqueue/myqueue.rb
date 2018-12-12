9509c4e4ea7c727673199bdb4e2d501f6255893d
Jose Mena <josemena0114@gmail.com>
2018-12-12 17:28:59 -0500

    queue works

--- 01-data-structures/02-stacks-and-queues/myqueue/myqueue.rb
+++ 01-data-structures/02-stacks-and-queues/myqueue/myqueue.rb
@@ -7,12 +7,23 @@ class MyQueue
     @head = @queue[0]
   end
 
+
+#Uses insert array function to add element at index -1, which would be the last element in the array
   def enqueue(element)
+   @queue.insert(-1, element)
+   @tail = @queue[-1]
+   @head = @queue[0]
   end
 
+#uses delete_at to remove the first element in the array
   def dequeue
+    @queue.delete_at(0)
+    @head = @queue[0]
+    @tail = @queue[-1]
   end
 
+#returns true if the array length is zero which indicates that it is empty
   def empty?
+    @queue.length == 0
   end
 end
