1db37c4d64b6a510f3a14ddec5f61955410e27a9
Jose Mena <josemena0114@gmail.com>
2018-12-12 17:34:18 -0500

    final with comments

--- 01-data-structures/02-stacks-and-queues/mystack/mystack.rb
+++ 01-data-structures/02-stacks-and-queues/mystack/mystack.rb
@@ -6,11 +6,13 @@ class MyStack
     self.top = nil
   end
 
+#adds item to the "top" which would be the back of the array using shovel operator
   def push(item)
     self.top = item
     @stack << item
   end
 
+#pop function removes the last item in a given array and returns it. Here used the delete_at function at index -1 and returned that item
   def pop
     last_in = @stack.delete_at(-1)
     self.top = @stack[-1]
