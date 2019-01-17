64a3a514fa1d4fe82f744f286e9301f96846728b
Jose Mena <josemena0114@gmail.com>
2019-01-16 22:01:21 -0500

    passes tests

--- 01-data-structures/04-hashes-part-1/hashclass.rb
+++ 01-data-structures/04-hashes-part-1/hashclass.rb
@@ -2,27 +2,38 @@ class HashClass
 
   def initialize(size)
     @items = Array.new(size)
-    @size = size
   end
 
 #setter? @items.size
-  def []=(key, value)
-    new_entry = HashItem.new(key, value)
-    @items[index(key, @items.size)] = new_entry
+def []=(key, value)
+  new_entry = HashItem.new(key, value)
+  entry = index(key,size)
+  if @items[entry] == nil  || @items[entry].value  == value
+    @items[entry] = new_entry
+  elsif @items[entry].key != new_entry.key
+    self.resize
+    self[key] = value
+  else @items[entry].value != new_entry.value
+    self.resize
+    @items[entry] = value
   end
+end
 
 #getter
   def [](key)
-  @items[index(key, @items.size)].value
+  @items[self.index(key, self.size)].value
 
   end
 
 #resize works
   def resize
-    @size = @size * 2
-    new_hash = Array.new(@size)
+    small_hash = @items.compact #compact removes nil and the error related to calling .key or .value on a nil array element!!!
+    @items = Array.new(self.size * 2)
+    small_hash.each do |entry|
+        self[entry.key] = entry.value
+      end
+   end
 
-  end
 
   # Returns a unique, deterministically reproducible index into an array
   # We are hashing based on strings, let's use the ascii value of each string as
@@ -33,7 +44,7 @@ class HashClass
 
   # Simple method to return the number of items in the hash
   def size
-   @size
+   @items.length
   end
 
 end
