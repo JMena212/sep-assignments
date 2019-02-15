e4fadeb5dbc8a2828291872644e267d101f2441a
Jose Mena <josemena0114@gmail.com>
2019-02-15 09:47:36 -0500

    node and insert

--- 01-data-structures/06-trees/binary_tree/binary_search_tree.rb
+++ 01-data-structures/06-trees/binary_tree/binary_search_tree.rb
@@ -3,9 +3,23 @@ require_relative 'node'
 class BinarySearchTree
 
   def initialize(root)
+    @root = root
   end
 
   def insert(root, node)
+    if node.rating <= root.rating
+      if root.left = nil
+        node = root.left
+      else
+        insert (root.left, node)
+      end
+    else node.rating > root.rating
+      if root.right = nil
+        node = root.right
+      else
+        insert(root.right, node)
+      end
+    end
   end
 
   # Recursive Depth First Search
