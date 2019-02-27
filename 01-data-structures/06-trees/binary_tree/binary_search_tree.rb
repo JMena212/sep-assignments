422b6644eb741a90fdd97536bb2e5b3a32f84e59
Jose Mena <josemena0114@gmail.com>
2019-02-27 18:18:54 -0500

    delete works

--- 01-data-structures/06-trees/binary_tree/binary_search_tree.rb
+++ 01-data-structures/06-trees/binary_tree/binary_search_tree.rb
@@ -27,12 +27,38 @@ attr_accessor :root
 
   # Recursive Depth First Search
   def find(root, data)
+    if root && data
+      if root.title == data
+        return root
+      elsif root.left
+        find(root.left, data)
+      elsif root.right
+        find(root.right, data)
+      end
+    else root == nil || data == nil
+      return nil
+    end
+    #go as far left as you can then right, use recursion
+    #call left if you can otherwise call righ
   end
 
   def delete(root, data)
+    if root == nil || data == nil
+      return nil
+    else
+      delete_node = find(root, data)
+      if delete_node
+        delete_node.title = nil
+        delete_node.rating = nil
+      else
+        return nil
+      end
+    end
   end
 
   # Recursive Breadth First Search
   def printf(children=nil)
+    #use a queue to track each node as it goes down each layer
+    #start with root and go down each layer
   end
 end
