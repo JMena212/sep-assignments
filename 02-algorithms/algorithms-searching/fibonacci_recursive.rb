c9d41a9a5e7397f4074af3fa01101eb1b43da339
Jose Mena <josemena0114@gmail.com>
2019-05-23 21:22:34 -0400

    finished with fibonacci recursive responses

--- 02-algorithms/algorithms-searching/fibonacci_recursive.rb
+++ 02-algorithms/algorithms-searching/fibonacci_recursive.rb
@@ -11,10 +11,20 @@ def fib(n)
    end
  end
 
-
+*/
 1. Given an unsorted collection of a million items,
 which algorithm would you choose between linear search and binary search?
 Would you use an iterative or recursive solution? Explain your reasoning.
 
- In this case I would use a linear solution iterative search. Binary searches require sorted
-collections and wouldn't be helpful in this example. 
+In this case I would use a linear search iterative solution. Binary searches require sorted
+collections and wouldn't be helpful in this example. Recursion also takes much longer to process
+with large amounts of data.
+
+2.Given a sorted collection of a million items,
+which algorithm would you choose between linear search and binary search?
+Would you use an iterative or recursive solution? Explain your reasoning.
+
+In a sorted collection of a million items, I would use a binary search using recursion.
+
+
+/*
