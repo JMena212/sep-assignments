02880f9c111a32297890189283c078cbef41da76
Jose Mena <josemena0114@gmail.com>
2019-07-12 15:31:09 -0400

    route fully functioning

--- 02-algorithms/complex-problems/tsp_greed.rb
+++ 02-algorithms/complex-problems/tsp_greed.rb
@@ -1,40 +1,78 @@
+$visited = []
+
 class City
 
   attr_accessor  :city
-  attr_accessor  :distances
   attr_accessor  :visited
   attr_accessor  :neighbors
 
   def initialize(city)
     @city = city
-    @distances = []
-    @neighbors = []
-    @visited = []
+    @neighbors = Hash.new
   end
+end
 
 
-  def nearest_possible_neighbor(current_city)
-    #SET neighbor_cities TO current_city.neighbors
-    neighbor_cities = current_city.neighbors
+#RETURNS A CITY CLASS OBJECT.
+def nearest_possible_neighbor(current_city)
+  current_city.neighbors
+  neighbor_cities = current_city.neighbors
 
-    # SET nearest_neighbor TO first element in
-    neighbor_cities = nearest_neighbor[0]
 
-    #FOR current_neighbor IN neighbor_cities
-    for current_neighbor in neighbor_cities
+  #*SORT NEIGHBOR CITIES BY DISTANCE FROM CURRENT CITY
+  sorted_cities = neighbor_cities.sort_by{|k,v| v[:distance] }
 
-      # IF current_neighbor.visited IS FALSE AND current_neighbor.distance < nearest_neighbor.distance
-      if current_neighbor.visited == false && current_neighbor.distance < nearest_neighbor.distance
-        #  ASSIGN nearest_neighbor TO current_neighbor
-        nearest_neighbor = current_neighbor
-        #END IF
-      end
-      #END FOR
-    end
-    nearest_neighbor
+
+  i = 0
+
+  while $visited.include?(sorted_cities[i][1][:city].city) == true do
+    i += 1
   end
-
-
+  next_city = sorted_cities[i][1][:city]
 
 
 end
+
+
+def route(starting_city)
+
+  total_stops = starting_city.neighbors.length + 1
+  current_city = starting_city
+  $visited.push(starting_city.city)
+
+
+  until $visited.length == total_stops  do
+    next_stop = nearest_possible_neighbor(current_city)
+    $visited << next_stop.city
+    current_city = next_stop
+    puts next_stop.city
+  end
+
+  #takes the salesman back home!
+  if $visited.length == total_stops
+    $visited.push(starting_city.city)
+  end
+
+  return $visited
+
+end
+
+
+new_york = City.new("New York")
+seattle = City.new("Seattle")
+oakland = City.new("Oakland")
+miami = City.new("Miami")
+santo_domingo = City.new("Santo Domingo")
+
+new_york.neighbors = {"Seattle" => {distance: 2852, city: seattle} ,"Oakland" => {distance: 2896, city: oakland}, "Miami" => {city: miami, distance: 1288},"Santo Domingo" => {distance: 1554, city: santo_domingo}}
+
+seattle.neighbors = {"New York" => {city: new_york, distance: 2852}, "Oakland" => {city: oakland, distance: 800},"Miami" =>{city: miami, distance: 3300}, "Santo Domingo" =>{ city: santo_domingo, distance: 3547 }}
+
+oakland.neighbors ={"New York" => {city: new_york, distance: 2896} , "Seattle" => {distance: 800, city: seattle}, "Miami" => {city: miami, distance: 3100}, "Santo Domingo" =>{ city: santo_domingo, distance: 3400} }
+
+miami.neighbors = {"New York" => {city: new_york, distance: 1288}, "Seattle" => {distance: 3300, city: seattle}, "Oakland" => {distance: 3100, city: oakland}, "Santo Domingo" => {distance: 824, city: santo_domingo}}
+
+santo_domingo.neighbors = {"New York" => {city: new_york, distance: 1554}, "Seattle" => {distance: 3547, city: seattle}, "Oakland" => {city: oakland, distance: 3400}, "Miami" => {city: miami, distance: 824}}
+
+
+k = route(oakland)
